e3dfb80ac5a5cc788974eaa8d135e4ce9f0dd5db
EricCRodriguez <ecrod12@gmail.com>
2021-03-18 20:26:10 -0500

    added Navbar components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { GlobalStyle } from "./globalStyles";
+import Hero from "./components/Hero";
+import Products from "./components/Products";
+import { productData, productDataTwo } from "./components/Products/data";
+import Footer from "./components/Footer";
+import Feature from "./components/Feature";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Pizza Shop</h1>
-    </div>
+    <Router>
+      <GlobalStyle />
+      <Hero />
+      <Products heading="Choose your favorite" data={productData} />
+      <Feature />
+      <Products heading="Sweet Treats for You" data={productDataTwo} />
+      <Footer />
+    </Router>
   );
 }
 
